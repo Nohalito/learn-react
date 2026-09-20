f416aec9e3204c0dabe6564ce3ab4fdb16e36651
Nohalito <159722918+Nohalito@users.noreply.github.com>
2026-09-20 19:23:09 +0200

    tictactoe-game

---
+++ tictactoe-game/src/components/Square.tsx
@@ -0,0 +1,18 @@
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
+
+function Square({ value, onSquareClick }: SquareProps) {
+
+  return (
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default Square;
